791c0718c99368164513df3b96a9dc5db0142876
garrett cullen <info@garrettcullen.com>
2016-06-01 09:56:17 -0700

    push

--- wp-content/themes/studiobarre-corporate/single-press.php
+++ wp-content/themes/studiobarre-corporate/single-press.php
@@ -11,7 +11,7 @@ get_header(); ?>
 
 
 <div style="height:200px;margin-top:45px;">
-To View, go to the <a href="<?php bloginfo('url');?>/press">Press Page</a>
+To add this to the Press Page, go to the <a href="<?php bloginfo('url');?>/wp-admin/post.php?post=850&action=edit">Press Page</a>
 </div>
 
 <?php get_footer(); ?>
